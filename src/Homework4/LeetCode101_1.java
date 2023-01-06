a203eec15154b65948b6be816aef348e5c8feb80
Alona Ratsebarska <aleonorarace@gmail.com>
2023-01-06 16:14:33 +0100

    Fourth Homework

---
+++ src/Homework4/LeetCode101_1.java
@@ -0,0 +1,37 @@
+package Homework4;
+
+public class LeetCode101_1 {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null)return true;
+        if(root.left == null && root.right == null)return true;
+        if(root.left == null || root.right == null)return false;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root.left);
+        stack.push(root.right);
+        while(!stack.isEmpty()){
+            TreeNode right = null;
+            TreeNode left = null;
+            if(!stack.isEmpty()){
+                right = stack.pop();
+            }
+            if(!stack.isEmpty()){
+                left = stack.pop();
+            }
+            if(left == null || right == null)return false;
+            if(left.val != right.val)return false;
+            if(left.left != null && right.right != null){
+                stack.push(left.left);
+                stack.push(right.right);
+            }
+            else if(!(left.left == null && right.right == null))return false;
+
+            if(left.right != null && right.left != null){
+                stack.push(left.right);
+                stack.push(right.left);
+            }
+            else if(!(left.right == null && right.left == null))return false;
+
+        }
+        return true;
+    }
+}
